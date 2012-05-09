bd7f57102fcbc77a7f232bea432bf717de4838f7
etherglow <etherglow@gmail.com>
2012-05-09 10:14:02 +0700

    Full functionality of parameter passing on Verification Form. VF now uploads both template and feature name. featureName computed from hash of IP, templateName from HTML input.

--- AppletFingerprint/src/com/budiw/fpapplet/VerificationForm.java
+++ AppletFingerprint/src/com/budiw/fpapplet/VerificationForm.java
@@ -2,6 +2,9 @@ package com.budiw.fpapplet;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
 
 import javax.swing.JOptionPane;
 
@@ -24,21 +27,35 @@ public class VerificationForm extends CaptureForm
 {
 	private static final long serialVersionUID = -9160876912046100450L;
 	private static final String DEFAULT_FEATURE_NAME = "inputfeature";
-	public static final String DEFAULT_FEATURE_PATH = System.getProperty("user.home") + "\\"  + DEFAULT_FEATURE_NAME + ".fpp";
+//	public static final String DEFAULT_FEATURE_PATH = System.getProperty("user.home") + "\\"  + DEFAULT_FEATURE_NAME + ".fpp";
 	private static final String VERIFICATION_KEYWORD = "VERIFIED";
 	private String templateName = "";
-	
-	
+	private String featureName = DEFAULT_FEATURE_NAME;
+	private String featurePath = "";
+
+
 	public VerificationForm(String templateName) {
-		super();
+		this();
 		if(templateName == "" || templateName == null)
 			this.templateName = EnrollmentForm.DEFAULT_TEMPLATE_NAME;
 		else
 			this.templateName = templateName;
-		
-//		featurePath = System.getProperty("user.home") + "\\"  + filename + ".fpp";
+
+		//featureName is computed by the hash of the client's IP so that it's unique and obscure
+		InetAddress thisIp = null;
+		int featureNumber = 0;
+		Random rand = new Random();
+		try {
+			thisIp = InetAddress.getLocalHost();
+			featureNumber = thisIp.getHostAddress().hashCode();
+		} catch (UnknownHostException e1) {
+			featureNumber = rand.nextInt();
+			e1.printStackTrace();
+		} 
+		featureName = Integer.toString(featureNumber);
+		featurePath = System.getProperty("user.home") + "\\"  + featureName + ".fpp";
 	}
-	
+
 	public VerificationForm() {
 		super();
 	}
@@ -60,7 +77,7 @@ public class VerificationForm extends CaptureForm
 		if (features != null)
 		{
 			//Write to file and upload to server for comparison. Verification format must be *.fpp
-			writeFile(DEFAULT_FEATURE_PATH, features.serialize());
+			writeFile(featurePath, features.serialize());
 			String response = uploadFeature();
 			if(response.indexOf(VERIFICATION_KEYWORD) != -1) {
 				JOptionPane.showMessageDialog(this, "Verified");
@@ -69,48 +86,47 @@ public class VerificationForm extends CaptureForm
 				JOptionPane.showMessageDialog(this, "DENIED", "FAILED VERIFICATION", JOptionPane.ERROR_MESSAGE);
 		}
 	}
-	
+
 	private void updateStatus(int FAR)
 	{
 		// Show "False accept rate" value
 		setStatus(String.format("False Accept Rate (FAR) = %1$s", FAR));
 	}
-	
+
 	@SuppressWarnings("unused")
 	protected String uploadFeature() {
 		HttpClient httpclient = new DefaultHttpClient();
 		httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
 
 		HttpPost httppost = new HttpPost("https://localhost/upload.php");
-//		BasicHttpParams fileParam = new BasicHttpParams();
-//		fileParam.setParameter("templateName", filename);
-//		httppost.setParams(fileParam);
-		File file = new File(DEFAULT_FEATURE_PATH);
+		//		BasicHttpParams fileParam = new BasicHttpParams();
+		//		fileParam.setParameter("templateName", filename);
+		//		httppost.setParams(fileParam);
+		File file = new File(featurePath);
 
 		//This will appear on $_FILES, uploading the challenger's fingerprint feature
 		MultipartEntity mpEntity = new MultipartEntity();
 		ContentBody cbFile = new FileBody(file, "image/jpeg");
 		mpEntity.addPart("userfile", cbFile);
-		
-		//This will appear on $_POST, uploading templateName to be matched against the input feature
-		ContentBody tName = null;
+
+		//This will appear on $_POST, uploading templateName & featureName to be matched
 		try {
-			tName = new StringBody(templateName);
+			ContentBody tName = new StringBody(templateName);
+			ContentBody fName = new StringBody(featureName);
+			mpEntity.addPart("templateName", tName);
+			mpEntity.addPart("featureName", fName);
 		} catch (UnsupportedEncodingException e1) {
 			e1.printStackTrace();
 		}
-		mpEntity.addPart("templateName", tName);
-
-
+		
 		httppost.setEntity(mpEntity);
 		//		System.out.println("executing request " + httppost.getRequestLine());
 
 		try {
 			HttpResponse response = httpclient.execute(httppost);
 			HttpEntity resEntity = response.getEntity();
-			
-//			System.out.println(response.getStatusLine());
-			
+			//			System.out.println(response.getStatusLine());
+
 			//Print content of response, check for verification message
 			if (resEntity != null) {
 				String responsePayload = EntityUtils.toString(resEntity);
@@ -127,7 +143,7 @@ public class VerificationForm extends CaptureForm
 		httpclient.getConnectionManager().shutdown();
 		//When response is null, return empty String
 		return "";
-		
+
 	}
 
 }
